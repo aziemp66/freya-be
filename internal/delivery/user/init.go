5670545fbdf4b86f8f5b9efae95d11e04bc03796
Azie Melza Pratama <aziemp66@gmail.com>
2023-03-11 20:47:09 +0700

    Adding Login Adnd Register Delivery

--- internal/delivery/user/init.go
+++ internal/delivery/user/init.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	httpCommon "github.com/aziemp66/freya-be/common/http"
 	userUserCase "github.com/aziemp66/freya-be/internal/usecase/user"
 	"github.com/gin-gonic/gin"
 )
@@ -14,5 +15,49 @@ func NewUserDelivery(router *gin.RouterGroup, userUseCase userUserCase.Usecase) 
 		UserUseCase: userUseCase,
 	}
 
+	router.POST("/login", UserDelivery.Login)
+	router.POST("/register", UserDelivery.Register)
+
 	return UserDelivery
+}
+
+func (u *UserDelivery) Login(c *gin.Context) {
+	var loginRequest httpCommon.Login
+
+	if err := c.ShouldBindJSON(&loginRequest); err != nil {
+		return
+	}
+
+	token, err := u.UserUseCase.Login(c, loginRequest.Email, loginRequest.Password)
+
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(200, httpCommon.Response{
+		Code:    200,
+		Message: "Login success",
+		Value:   gin.H{"token": token},
+	})
+}
+
+func (u *UserDelivery) Register(c *gin.Context) {
+	var registerRequest httpCommon.AddUser
+
+	if err := c.ShouldBindJSON(&registerRequest); err != nil {
+		return
+	}
+
+	err := u.UserUseCase.Register(c, registerRequest)
+
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(200, httpCommon.Response{
+		Code:    200,
+		Message: "Register success",
+	})
 }
